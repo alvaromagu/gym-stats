d9eb102b4449d6aa63ca8e4169cb7f3fc30dd7ae
alvaro <alvaromagu2002@gmail.com>
2025-09-28 01:26:14 +0200

    feat: refactor login and register pages to use Card component for improved layout

--- packages/web/src/auth/pages/login.tsx
+++ packages/web/src/auth/pages/login.tsx
@@ -3,30 +3,44 @@ import { Input } from '@/shared/components/ui/input';
 import { Label } from '@/shared/components/ui/label';
 import { Loader2Icon } from 'lucide-react';
 import { useLogin } from '@/auth/hooks/login';
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from '@/shared/components/ui/card';
 
 export function LoginPage() {
   const { loading, handleSubmit } = useLogin();
 
   return (
     <main className='p-2'>
-      <h1 className='text-center text-4xl font-extrabold text-balance mb-5'>
-        Iniciar sesión
-      </h1>
-      <form className='flex flex-col gap-6' onSubmit={handleSubmit}>
-        <Label className='flex flex-col items-start gap-3'>
-          Correo electrónico
-          <Input
-            placeholder='Escribe tu correo electrónico aquí'
-            type='email'
-            name='email'
-            required
-          />
-        </Label>
-        <Button type='submit' disabled={loading}>
-          {loading && <Loader2Icon className='animate-spin size-6' />}
-          Iniciar sesión
-        </Button>
-      </form>
+      <Card>
+        <CardHeader>
+          <CardTitle>
+            <h1 className='text-3xl font-extrabold text-balance'>
+              Iniciar sesión
+            </h1>
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form className='flex flex-col gap-6' onSubmit={handleSubmit}>
+            <Label className='flex flex-col items-start gap-3'>
+              Correo electrónico
+              <Input
+                placeholder='Escribe tu correo electrónico aquí'
+                type='email'
+                name='email'
+                required
+              />
+            </Label>
+            <Button type='submit' disabled={loading}>
+              {loading && <Loader2Icon className='animate-spin size-6' />}
+              Iniciar sesión
+            </Button>
+          </form>
+        </CardContent>
+      </Card>
     </main>
   );
 }
